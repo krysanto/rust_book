12a270e20ef42990e337a75c96928e13d8462f8e
Michael Csida <krysanto@gmail.com>
2022-07-06 11:09:55 +0200

    Finished Chapter 5

---
+++ chapter5/structs/target/debug/deps/structs-30e21676146f26fd.d
@@ -0,0 +1,5 @@
+/home/krysanto/rust_book/chapter5/structs/target/debug/deps/structs-30e21676146f26fd.rmeta: src/main.rs
+
+/home/krysanto/rust_book/chapter5/structs/target/debug/deps/structs-30e21676146f26fd.d: src/main.rs
+
+src/main.rs:
---
+++ chapter5/structs/target/debug/deps/structs-dea3bae1373e8550.d
@@ -0,0 +1,5 @@
+/home/krysanto/rust_book/chapter5/structs/target/debug/deps/structs-dea3bae1373e8550.rmeta: src/main.rs
+
+/home/krysanto/rust_book/chapter5/structs/target/debug/deps/structs-dea3bae1373e8550.d: src/main.rs
+
+src/main.rs:
