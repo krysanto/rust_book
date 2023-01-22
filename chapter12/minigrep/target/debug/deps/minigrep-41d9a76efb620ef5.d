8e2257755c22a77bc8a3bd496d4791d5ce944e4f
Michael Csida <krysanto@gmail.com>
2023-01-23 00:42:14 +0100

    Started up chapter12

---
+++ chapter12/minigrep/target/debug/deps/minigrep-41d9a76efb620ef5.d
@@ -0,0 +1,5 @@
+/home/krysanto/rust_book/chapter12/minigrep/target/debug/deps/minigrep-41d9a76efb620ef5: src/main.rs
+
+/home/krysanto/rust_book/chapter12/minigrep/target/debug/deps/minigrep-41d9a76efb620ef5.d: src/main.rs
+
+src/main.rs:
