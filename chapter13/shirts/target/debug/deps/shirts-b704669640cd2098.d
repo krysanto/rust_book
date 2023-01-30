578e86792b153e38a263bbedc7b3dc3316f638d2
Michael Csida <krysanto@gmail.com>
2023-01-30 15:24:27 +0100

    Finished Chapter 13

---
+++ chapter13/shirts/target/debug/deps/shirts-b704669640cd2098.d
@@ -0,0 +1,5 @@
+/home/krysanto/rust_book/chapter13/shirts/target/debug/deps/shirts-b704669640cd2098: src/main.rs
+
+/home/krysanto/rust_book/chapter13/shirts/target/debug/deps/shirts-b704669640cd2098.d: src/main.rs
+
+src/main.rs:
---
+++ chapter13/shirts/target/debug/shirts.d
@@ -0,0 +1,1 @@
+/home/krysanto/rust_book/chapter13/shirts/target/debug/shirts: /home/krysanto/rust_book/chapter13/shirts/src/main.rs
